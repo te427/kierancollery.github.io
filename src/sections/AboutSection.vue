20bff83f5e5894ad4f8bb4b5b1577c69172312cc
kieran <kieran@8afb8325.st-andrews.ac.uk>
2026-09-02 13:48:25 +0100

    Add image and frame style to about me section

--- src/sections/AboutSection.vue
+++ src/sections/AboutSection.vue
@@ -1,10 +1,72 @@
 <!-- This section should contain a self-portrait (style?) and writeup about myself -->
-<script>
-</script>
+<script></script>
 
 <template>
-    TODO: ABOUT
+  <div class="about-container">
+    <div class="img-container">
+      <div class="img-frame">
+        <img src="/website_self_cropped.jpg" class="self" />
+      </div>
+    </div>
+    <div class="text-container">ABOUT ME</div>
+  </div>
 </template>
 
 <style scoped>
+.about-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center; /* Horizontal center */
+  align-items: center; /* Vertical center */
+}
+
+.img-container {
+  display: flex;
+  justify-content: center; /* Horizontal center */
+  align-items: center; /* Vertical center */
+}
+
+.img-frame {
+  width: 40vw;
+  aspect-ratio: 1/1;
+  overflow: hidden;
+  position: relative;
+}
+
+/* Fade all edges to white using a radial gradient */
+.img-frame::after {
+  content: '';
+  position: absolute;
+  inset: 0;
+  background: radial-gradient(circle at center, transparent 55%, white 65%);
+  pointer-events: none;
+}
+
+.self {
+  border: solid 20px white;
+}
+
+/* Touchscreen () */
+@media (max-width: 600px) {
+  .img-frame {
+    width: 100vw;
+  }
+
+  .self {
+    max-width: 100vw;
+  }
+
+  .mobile-only {
+    display: block;
+  }
+}
+
+@media (min-width: 601px) {
+  .self {
+    max-width: 40vw;
+  }
+  .desktop-only {
+    display: block;
+  }
+}
 </style>
