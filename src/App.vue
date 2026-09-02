821d8a55ecf2b42b2ae45aecd3288f9b20d0982d
kieran <kieran@8afb8325.st-andrews.ac.uk>
2026-09-02 14:39:23 +0100

    added text and style for About

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <!-- This section should provide a "Return to top" button and disclaimers -->
 <script setup lang="ts">
-import {useTemplateRef, onMounted} from 'vue'
+import { useTemplateRef, onMounted } from 'vue'
 import Section from './sections/Section.vue'
 import LogoSection from './sections/LogoSection.vue'
 import AboutSection from './sections/AboutSection.vue'
@@ -8,16 +8,16 @@ import MapSection from './sections/MapSection.vue'
 import ResearchSection from './sections/ResearchSection.vue'
 import ContactSection from './sections/ContactSection.vue'
 
-const createSections = (components) => components.map((component, id) => ({id, component}))
-let returnHidden = true;
-let returnButton = useTemplateRef('return-button')
+const createSections = (components) => components.map((component, id) => ({ id, component }))
+let returnHidden = true
+const returnButton = useTemplateRef('return-button')
 
 const components = createSections([
   LogoSection,
   AboutSection,
   MapSection,
   ResearchSection,
-  ContactSection
+  ContactSection,
 ])
 
 onMounted(() => {})
@@ -28,7 +28,7 @@ const hidingClass = 'return-hiding'
 document.body.onscroll = (e) => {
   if (!window.scrollY) {
     // hide return button
-    returnHidden = true 
+    returnHidden = true
     returnButton.value.classList.remove(showingClass)
     returnButton.value.classList.add(hidingClass)
   }
@@ -38,20 +38,17 @@ document.body.onscroll = (e) => {
     returnButton.value.classList.remove(hidingClass)
     returnButton.value.classList.add(showingClass)
   }
-
 }
 </script>
 
 <template>
   <div class="scroll-container">
-    <Section v-for="({component, id}, _) in components" :key="id">
-      <component :is="component"/>
+    <Section v-for="({ component, id }, _) in components" :key="id">
+      <component :is="component" />
     </Section>
   </div>
 
-  <div ref="return-button" class="return-to-top">
-    Return to top
-  </div>
+  <div ref="return-button" class="return-to-top">Return to top</div>
 </template>
 
 <style>
--- src/sections/AboutSection.vue
+++ src/sections/AboutSection.vue
@@ -5,25 +5,44 @@
   <div class="about-container">
     <div class="img-container">
       <div class="img-frame">
-        <img src="/website_self_cropped.jpg" class="self" />
+        <img class="img-content" src="/website_self_cropped.jpg" />
       </div>
     </div>
-    <div class="text-container">ABOUT ME</div>
+    <div class="text-container">
+      <p class="text-title">Hey there!</p>
+      <p class="text-content">
+        My name is Kieran Collery, and I am a University of Oxford DPhil candidate in Theology and
+        Religion, currently studying the philosophical works of Lev Shestov and his assessment and
+        critiques of modern Lutheran thought.
+      </p>
+      <p class="text-content">
+        My current topics of interest include the relationship between faith and reason, the
+        phenomenon of play, the encounter with otherness, and the irruption of incongruous laughter,
+        all with a view to their application to Christian theology.
+      </p>
+      <p class="text-content">
+        Previously, I studied theology at Durham and St. Andrews. Prior to my academic life, I
+        worked as a software engineer. I am grateful to be able to call myself a Christian, and hope
+        that my work may help to enrich the life of the Church catholic and those who knock upon
+        God's door.
+      </p>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .about-container {
   display: flex;
-  flex-direction: column;
-  justify-content: center; /* Horizontal center */
-  align-items: center; /* Vertical center */
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: #fdfdfd;
 }
 
 .img-container {
   display: flex;
-  justify-content: center; /* Horizontal center */
-  align-items: center; /* Vertical center */
+  justify-content: center;
+  align-items: center;
 }
 
 .img-frame {
@@ -33,40 +52,54 @@
   position: relative;
 }
 
-/* Fade all edges to white using a radial gradient */
+/* Fade edges to radial gradient for frame. */
 .img-frame::after {
   content: '';
   position: absolute;
   inset: 0;
-  background: radial-gradient(circle at center, transparent 55%, white 65%);
+  background: radial-gradient(circle at center, transparent 55%, #fdfdfd 60%);
   pointer-events: none;
 }
 
-.self {
+.img-content {
   border: solid 20px white;
 }
 
-/* Touchscreen () */
+.text-container {
+  font-family: 'Geist', Helvetica, sans-serif;
+  color: #555555;
+  max-width: 40vw;
+}
+
+.text-title {
+  font-family: 'Geist Pixel', sans-serif;
+  font-size: 24px;
+  color: black;
+  font-weight: bold;
+}
+
+/* Touchscreen (mobile/tablet) */
 @media (max-width: 600px) {
   .img-frame {
     width: 100vw;
   }
 
-  .self {
+  .img-content {
     max-width: 100vw;
   }
 
-  .mobile-only {
-    display: block;
+  .about-container {
+    flex-direction: column;
+  }
+
+  .text-container {
+    max-width: 85vw;
   }
 }
 
 @media (min-width: 601px) {
-  .self {
+  .img-content {
     max-width: 40vw;
-  }
-  .desktop-only {
-    display: block;
   }
 }
 </style>
