3613b77789fc3168d78836ea0252b1f61c535f6b
kieran <kieran@command-center.local>
2026-09-15 01:23:55 +0100

    added back base path in vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
-  base: '/',
+  base: 'kierancollery.github.io',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
