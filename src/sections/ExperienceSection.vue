4d13f23986a8c579c16d5c4621229ccdc151e133
kieran <kieran@command-center.local>
2026-09-26 13:34:05 +0100

    updated st andrews education profile

--- src/sections/ExperienceSection.vue
+++ src/sections/ExperienceSection.vue
@@ -83,7 +83,7 @@ const contentMap: Content = {
         'Focused historical and philosophical theology, culminating in a dissertation supervised by Prof. Judith Wolfe ' +
           "synthesising a model of the Lord's Supper in terms of the aesthetic philosophy of Hans-Georg Gadamer and the hermeneutic theology " +
           'of Eberhard Jüngel.',
-        'Graduated with distinction.',
+        "Graduated with distinction and awarded a place on the Dean's List.",
       ],
     },
     [AcademicItems.Durham]: {
